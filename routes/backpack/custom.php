8802dcea68ad501fc032cbc3f3c30cd3285cae83
tmStore <tmstore@iMac-tmStore.local>
2019-06-11 17:14:49 +0500

    routes

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -11,23 +11,5 @@ Route::group([
     'middleware' => ['web', config('backpack.base.middleware_key', 'admin')],
     'namespace'  => 'App\Http\Controllers\Admin',
 ], function () { // custom admin routes
-    CRUD::resource('category', 'CategoryCrudController');
-    CRUD::resource('material', 'MaterialCrudController');
-    CRUD::resource('order', 'OrderCrudController');
-    Route::get('/dashboard', 'AdminController@dashboard')->name('admin_dashboard');
-    Route::get('/stats/{id}', 'AdminController@cat_stats')->name('cat_stats');
-    CRUD::resource('menu', 'MenuCrudController');
-    CRUD::resource('slider', 'SliderCrudController');
-    CRUD::resource('advertisement', 'AdvertisementCrudController');
-    CRUD::resource('new', 'NewCrudController');
-    CRUD::resource('news', 'NewsCrudController');
-    CRUD::resource('show', 'ShowCrudController');
-    CRUD::resource('pageslider', 'PageSliderCrudController');
-    CRUD::resource('text', 'TextCrudController');
-    CRUD::resource('description', 'DescriptionCrudController');
-    CRUD::resource('page_ad', 'PageAdCrudController');
-    CRUD::resource('pagead', 'PageAdCrudController');
-    CRUD::resource('page', 'PageCrudController');
-    CRUD::resource('programme', 'ProgrammeCrudController');
     CRUD::resource('sample', 'SampleCrudController');
 }); // this should be the absolute last line of this file
